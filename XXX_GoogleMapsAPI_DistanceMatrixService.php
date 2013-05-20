22fd2b393c6fca5b708c7b7f0ce9bdd026b03a7d
vinceMeens <v.w.a.meens@comcordis.com>
2013-05-20 06:44:31 +0200

    Key issues fix 4

--- XXX_GoogleMapsAPI_DistanceMatrixService.php
+++ XXX_GoogleMapsAPI_DistanceMatrixService.php
@@ -18,7 +18,7 @@ class XXX_GoogleMapsAPI_DistanceMatrixService
 		$uri .= '&sensor=false';
 		if (self::$key != '')
 		{
-			$uri .= '&key=' . self::$key;
+			//$uri .= '&key=' . self::$key;
 		}
 		if ($languageCode != '')
 		{
@@ -61,7 +61,7 @@ class XXX_GoogleMapsAPI_DistanceMatrixService
 		$uri .= '&sensor=false';
 		if (self::$key != '')
 		{
-			$uri .= '&key=' . self::$key;
+			//$uri .= '&key=' . self::$key;
 		}
 		if ($languageCode != '')
 		{
